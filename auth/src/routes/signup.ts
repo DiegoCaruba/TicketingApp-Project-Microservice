622348dab99e9f6350bf319ab848af2093f7ceb5
Diego Caruba <diego.caruba.carmo@ibm.com>
2022-12-02 14:42:07 -0300

    feat: add sign in feature

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,8 +3,9 @@ import { body, validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middleware/validate-request';
+
 
 const router = express.Router();
 
@@ -18,13 +19,9 @@ router.post('/api/users/signup',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password must be between 4 and 20 characters')
     ],
+    validateRequest,
     async (req: Request, res: Response) => {
         const errors = validationResult(req);
-
-        // Check email and password rules
-        if (!errors.isEmpty()) {
-            throw new RequestValidationError(errors.array());
-        }
 
         // check if email is used
         const { email, password } = req.body;
