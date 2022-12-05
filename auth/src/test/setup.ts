3b14366ef29f881d7cf3cc861f26fb2e797cbdd9
Diego Caruba <diego.caruba.carmo@ibm.com>
2022-12-05 17:32:14 -0300

    test: add current user test suite

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,8 +1,13 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import app from '../app';
 
 let mongo: any;
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
 beforeAll(async () => {
     process.env.JWT_KEY = 'asdf';
@@ -27,3 +32,19 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email, password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+};
