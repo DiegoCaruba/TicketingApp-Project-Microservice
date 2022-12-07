074ef276564baf52377baf86875e09dd13a3f4be
Diego Caruba <diego.caruba.carmo@ibm.com>
2022-12-07 11:21:48 -0300

    feat: add hook and callback to signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,23 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
 
 const signup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email,
+            password
+        },
+        onSuccess: () => Router.push('/')
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-
-            console.log(response.data);
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+        doRequest();
     };
 
     return (
@@ -43,14 +44,7 @@ const signup = () => {
                 />
             </div>
 
-            {errors.length > 0 && <div className='alert alert-danger'>
-                <h4>Ooops...</h4>
-                <ul className='my-0'>
-                    {errors.map(err => (
-                        <li key={err.message}>{err.message}</li>
-                    ))}
-                </ul>
-            </div>}
+            {errors}
 
             <button className="btn btn-primary">Sign Up</button>
         </form>
